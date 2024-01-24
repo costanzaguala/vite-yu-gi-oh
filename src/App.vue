3c021f43eaa8e18cb7e93f13d447997b2556ef85
Costanza Guala <147189953+costanzaguala@users.noreply.github.com>
2024-01-24 16:15:53 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+
+export default {
+    data() {
+        return {
+             
+        };
+     },
+
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h1>
+        My App
+    </h1>
+
+    <AppHeader/>
+
+    <AppMain/>
+
+    <AppFooter/>
+
+</template>
+
+<style lang="scss">
+    @use "assets/scss/main.scss" as *;
+    @import "assets/scss/partials/reset.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+             
+        };
+     },
+    methods: {
+
+    }
+}
+</script>
+
+<template> 
+    <header>
+        Header
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
